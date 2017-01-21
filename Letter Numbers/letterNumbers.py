aad4a46560e1f193c3472ca465192971ba9b9377
Gurpartap Kaler <kaler.gurpartap@hotmail.com>
2017-01-20 23:03:02 -0500

    Add files via upload

---
+++ Letter Numbers/letterNumbers.py
@@ -0,0 +1,7 @@
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+numbers = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1]
+ln_dict = {}
+for i in range(len(letters)):
+    lp_dict[letters[i]] = numbers[i]
+
+
